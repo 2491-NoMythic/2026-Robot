105aefce18f8e58f4a79d822cfd4d87a73ed7563
Programming7 <2491NoMythic@gmail.com>
2026-02-17 16:55:29 -0600

    added import for new command

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -57,6 +57,7 @@ import frc.robot.Commands.Drive;
 import frc.robot.Commands.FeedShooter;
 import frc.robot.Commands.LightsCommand;
 import frc.robot.Commands.MoveToClimbingPose;
+import frc.robot.Commands.MoveToTrenchPose;
 import frc.robot.Commands.Outtake;
 import frc.robot.Commands.RunIntake;
 import frc.robot.Commands.RunShooterVelocity;
