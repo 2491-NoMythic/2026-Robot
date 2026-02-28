71e8099d105fa3e4f9cea14e5f20fed38ddd7e7f
Serena <serenathurlow55@gmail.com>
2026-02-27 19:31:35 -0600

    shooting speed in rps tuned

--- src/main/java/frc/robot/settings/Constants.java
+++ src/main/java/frc/robot/settings/Constants.java
@@ -64,6 +64,7 @@ public final class Constants {
 
   public static final class ShooterConstants{
     public static final float SHOOTING_SPEED_MPS = 7;//not measured, just example
+    public static final float SHOOTING_SPEED_RPS = 25;
     public static final int SHOOTER_LEFT_MOTOR_ID = 9; 
     public static final int SHOOTER_RIGHT_MOTOR_ID = 10; 
     public static final int HOOD_LEFT_ACTUATOR_ID = 2;
