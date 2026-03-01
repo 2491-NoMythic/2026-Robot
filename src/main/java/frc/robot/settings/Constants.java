a3db008b423ba2201dd606b551a72b1f71135c29
Zero-Zacks <zackoliversmith@gmail.com>
2026-03-01 12:24:00 -0600

    Prac field2 28 (#57)
    * tesing and tuning intake deployer limit swich configs - done!
    
    * added command that feeds the shooter while attempt to unstall the hopper motor if it stalls. Runs during a few autos for testing
    
    * fixed anlge conversion from math to Robot angle, fixed some radian/degrees conversions
    
    * callibrated some setpoint angles
    
    * shooting speed in rps tuned
    
    * made everything degrees
    
    * changed one last place where radians were used
    
    * one more spot
    
    * removed old constnat
    
    * reverted autos to normal feedshooter command
    
    * added hopper to intake command, enabled shootWhileMoving, added backwards to start of feedshooter, adjusted shoote rkp
    
    ---------
    
    Co-authored-by: GriffinBriggs <96089118+GriffinBriggs@users.noreply.github.com>
    Co-authored-by: Serena <serenathurlow55@gmail.com>
    Co-authored-by: prog02 <2491NoMythic@gmail.com>

--- src/main/java/frc/robot/settings/Constants.java
+++ src/main/java/frc/robot/settings/Constants.java
@@ -63,7 +63,7 @@ public final class Constants {
   }
 
   public static final class ShooterConstants{
-    public static final float SHOOTING_SPEED_MPS = 7;//not measured, just example
+    public static final float SHOOTING_SPEED_MPS = 7;
     public static final float SHOOTING_SPEED_RPS = 25;
     public static final int SHOOTER_LEFT_MOTOR_ID = 9; 
     public static final int SHOOTER_RIGHT_MOTOR_ID = 10; 
@@ -77,7 +77,7 @@ public final class Constants {
     public static final double HOOD_UP_POSITION = 0.8;
     public static TalonFXConfiguration SHOOTER_CONFIG = new TalonFXConfiguration()
       .withSlot0(new Slot0Configs()
-        .withKV(0.17).withKP(1).withKI(0).withKD(0).withKS(0.675))
+        .withKV(0.17).withKP(0.8).withKI(0).withKD(0).withKS(0.675))
       .withCurrentLimits(new CurrentLimitsConfigs()
         .withSupplyCurrentLimitEnable(true).withSupplyCurrentLimit(50))
       .withMotorOutput(new MotorOutputConfigs()
