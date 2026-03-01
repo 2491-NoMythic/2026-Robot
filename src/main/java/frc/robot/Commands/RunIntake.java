4e5b0b298205559e41c21839bc60ffea3d38dd2d
Zero-Zacks <zackoliversmith@gmail.com>
2026-03-01 12:24:00 -0600

    Prac field2 28 (#57)
    * tesing and tuning intake deployer limit swich configs - done!
    
    * added command that feeds the shooter while attempt to unstall the hopper motor if it stalls. Runs during a few autos for testing
    
    * fixed anlge conversion from math to Robot angle, fixed some radian/degrees conversions
    
    * callibrated some setpoint angles
    
    * shooting speed in rps tuned
    
    * made everything degrees
    
    * changed one last place where radians were used
    
    * one more spot
    
    * removed old constnat
    
    * reverted autos to normal feedshooter command
    
    * added hopper to intake command, enabled shootWhileMoving, added backwards to start of feedshooter, adjusted shoote rkp
    
    ---------
    
    Co-authored-by: GriffinBriggs <96089118+GriffinBriggs@users.noreply.github.com>
    Co-authored-by: Serena <serenathurlow55@gmail.com>
    Co-authored-by: prog02 <2491NoMythic@gmail.com>

--- src/main/java/frc/robot/Commands/RunIntake.java
+++ src/main/java/frc/robot/Commands/RunIntake.java
@@ -7,15 +7,20 @@ package frc.robot.Commands;
 import static frc.robot.settings.Constants.IntakeConstants.INTAKE_SPEED_RPS;
 
 import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.Hopper;
+import frc.robot.subsystems.Indexer;
 import frc.robot.subsystems.Intake;
 
 /* You should consider using the more terse Command factories API instead https://docs.wpilib.org/en/stable/docs/software/commandbased/organizing-command-based.html#defining-commands */
 public class RunIntake extends Command {
   Intake intake;
+  Hopper hopper;
+
   /** Creates a new Intake. */
-  public RunIntake(Intake intake) {
+  public RunIntake(Intake intake, Hopper hopper) {
     this.intake = intake;
-    addRequirements(intake);
+    this.hopper = hopper;
+    addRequirements(intake, hopper);
   }
 
   // Called when the command is initially scheduled.
@@ -26,12 +31,14 @@ public class RunIntake extends Command {
   @Override
   public void execute() {
     intake.setVelocity(INTAKE_SPEED_RPS);
+    hopper.feedIndexer();
   }
 
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
     intake.stopWheels();
+    hopper.stop();
   }
 
   // Returns true when the command should end.
