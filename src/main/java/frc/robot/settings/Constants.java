67d2210e05466a5b66baeb720eeac3a2928a4b5c
Programming7 <2491NoMythic@gmail.com>
2026-03-19 20:52:42 -0500

    tuned shooter speed

--- src/main/java/frc/robot/settings/Constants.java
+++ src/main/java/frc/robot/settings/Constants.java
@@ -68,8 +68,8 @@ public final class Constants {
   }
 
   public static final class ShooterConstants{
-    public static final float SHOOTING_SPEED_MPS = 7.3f;
-    public static final float SHOOTING_SPEED_RPS = 84f;
+    public static final float SHOOTING_SPEED_MPS = 7.6f;
+    public static final float SHOOTING_SPEED_RPS = 70;
     public static final int SHOOTER_LEFT_MOTOR_ID = 9; 
     public static final int SHOOTER_RIGHT_MOTOR_ID = 10; 
     public static final int HOOD_LEFT_ACTUATOR_ID = 2;
