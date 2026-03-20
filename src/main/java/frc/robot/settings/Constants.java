2393383d4f24d8dd791acd46d4cf09b131b36afd
Programming7 <2491NoMythic@gmail.com>
2026-03-19 20:52:42 -0500

    tuned shooter speed

--- src/main/java/frc/robot/settings/Constants.java
+++ src/main/java/frc/robot/settings/Constants.java
@@ -69,8 +69,13 @@ public final class Constants {
   }
 
   public static final class ShooterConstants{
+<<<<<<< HEAD
     public static final float SHOOTING_SPEED_MPS = 7.3f;
     public static final float SHOOTING_SPEED_RPS = SubsystemsEnabled.SAFE_MODE_IS_ON ? 20f : 84f;  // if safe mode is on the shooting speed will go down to maintain safety of little children
+=======
+    public static final float SHOOTING_SPEED_MPS = 7.6f;
+    public static final float SHOOTING_SPEED_RPS = 70;
+>>>>>>> 67d2210 (tuned shooter speed)
     public static final int SHOOTER_LEFT_MOTOR_ID = 9; 
     public static final int SHOOTER_RIGHT_MOTOR_ID = 10; 
     public static final int HOOD_LEFT_ACTUATOR_ID = 2;
